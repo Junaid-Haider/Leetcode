1993747a406062ab64c94f2d8a8f6500243f8aa6
Junaid Haider <www.iamboss7777@gmail.com>
2026-07-01 09:02:06 +0500

    Time: 0 ms (100%), Space: 12.2 MB (72.97%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(m==0){
+            nums1=nums2;
+            return;
+        }
+        if(n==0)
+            return;
+        int i=m-1;
+        int j=n-1;
+        for(int k=n+m-1;k>=0;k--){
+            if(j<0)
+                break;
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+        }
+    }
+};
