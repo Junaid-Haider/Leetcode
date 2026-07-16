3ba80d1c4b9c1a082f20e7247260d11dd9ce6c06
Junaid Haider <www.iamboss7777@gmail.com>
2026-07-16 09:10:17 +0500

    Time: 0 ms (100%), Space: 8.8 MB (68.68%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length=0;
+        int i;
+        for(i=s.size()-1;i>=0;i--){
+            if(s[i]!=' ')
+                break;
+        }
+        for(;i>=0;i--){
+            if(s[i]==' ')
+                break;
+            length++;
+        }
+        return length;
+    }
+};
